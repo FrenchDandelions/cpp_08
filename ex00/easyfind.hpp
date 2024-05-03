28748234ea87304985d414291175ba44bb50110e
LE Thomas <thomas.le.tle@gmail.com>
2024-05-03 03:00:56 +0200

    ex00 done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,4 +1,27 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+#include <iostream>
+#include <algorithm>
+#include <exception>
+
+# define RED "\033[1;31m"
+# define RESET "\033[0m"
+# define PURP "\033[1;95m"
+# define PURP2 "\033[1;35m"
+# define ORANGE "\033[1;91m"
+# define LIGHTB "\033[1;36m"
+# define BLUE "\033[1;94m"
+# define GREEN "\033[1;92m"
+# define YELLOW "\033[1;93m"
+
+template <typename T>
+int easyfind(T &container, int to_find)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), to_find);
+    if(it == container.end())
+        throw std::out_of_range("Value not found");
+    return(*it);
+}
+
 #endif
---
+++ ex00/main.cpp
@@ -0,0 +1,85 @@
+#include "easyfind.hpp"
+#include <vector>
+#include <deque>
+#include <list>
+
+int main()
+{
+    std::cout << std::endl << RED "---------------------------------------------------"<< std::endl;
+	std::cout <<"VECTOR CONTAINER TEST:" << std::endl;
+	std::cout << "---------------------------------------------------" RESET << std::endl;
+    std::vector<int> v;
+    for(int i = 1; i < 5; i++)
+    {
+        v.push_back(i);
+    }
+    std::cout << std::endl << GREEN "VALUES INSIDE THE VECTOR:" << std::endl;
+	std::cout << "---------------------------------------------------" RESET << std::endl;
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+    {
+        std::cout << "Value inside the container: " << *it << std::endl;
+    }
+    std::cout << std::endl << GREEN "USING EASYFIND TO FIND 4 AND 5:" << std::endl;
+	std::cout << "---------------------------------------------------" RESET << std::endl;
+    try
+    {
+        std::cout << easyfind(v, 4) << std::endl;
+        std::cout << easyfind(v, 5) << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Caught Exception: " << e.what() << std::endl;
+    }
+
+
+    std::cout << std::endl << RED "---------------------------------------------------"<< std::endl;
+	std::cout <<"DEQUE CONTAINER TEST:" << std::endl;
+	std::cout << "---------------------------------------------------" RESET << std::endl;
+    std::deque<int> deq;
+    for(int i = 1; i < 5; i++)
+    {
+        deq.push_back(i);
+    }
+    std::cout << std::endl << GREEN "VALUES INSIDE THE DEQUE:" << std::endl;
+	std::cout << "---------------------------------------------------" RESET << std::endl;
+    for(std::deque<int>::iterator it = deq.begin(); it != deq.end(); it++)
+    {
+        std::cout << "Value inside the container: " << *it << std::endl;
+    }
+    std::cout << std::endl << GREEN "USING EASYFIND TO FIND 4 AND 5:" << std::endl;
+	std::cout << "---------------------------------------------------" RESET << std::endl;
+    try
+    {
+        std::cout << easyfind(deq, 4) << std::endl;
+        std::cout << easyfind(deq, 5) << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Caught Exception: " << e.what() << std::endl;
+    }
+    std::cout << std::endl << RED "---------------------------------------------------"<< std::endl;
+	std::cout <<"LIST CONTAINER TEST:" << std::endl;
+	std::cout << "---------------------------------------------------" RESET << std::endl;
+    std::list<int> lst;
+    for(int i = 1; i < 5; i++)
+    {
+        lst.push_back(i);
+    }
+    std::cout << std::endl << GREEN "VALUES INSIDE THE LIST:" << std::endl;
+	std::cout << "---------------------------------------------------" RESET << std::endl;
+    for(std::list<int>::iterator it = lst.begin(); it != lst.end(); it++)
+    {
+        std::cout << "Value inside the container: " << *it << std::endl;
+    }
+    std::cout << std::endl << GREEN "USING EASYFIND TO FIND 4 AND 5:" << std::endl;
+	std::cout << "---------------------------------------------------" RESET << std::endl;
+    try
+    {
+        std::cout << easyfind(lst, 4) << std::endl;
+        std::cout << easyfind(lst, 5) << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Caught Exception: " << e.what() << std::endl;
+    }
+}
