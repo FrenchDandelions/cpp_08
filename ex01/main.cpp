b73697b0ef91108ca5636394654b89ee8f1ec3fa
LE Thomas <thomas.le.tle@gmail.com>
2024-05-03 04:23:04 +0200

    commented the printVector

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,23 +2,32 @@
 
 int main()
 {
-    Span sp = Span(5);
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
-    Span sp2 = Span(10000);
-    std::vector<int> v;
-    for(int i = 0; i < 10000; i++)
+    try
     {
-        v.push_back(i + 1);
+        Span sp = Span(5);
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+        Span sp2 = Span(10000);
+        std::vector<int> v;
+        for(int i = 0; i < 10000; i++)
+        {
+            v.push_back(i + 1);
+        }
+        sp2.addBunch(v.begin(), v.end());
+        // sp2.printVector();
+        std::cout << sp2.shortestSpan() << std::endl;
+        std::cout << sp2.longestSpan() << std::endl;
     }
-    sp2.addBunch(v.begin(), v.end());
-    // sp2.printVector();
-    std::cout << sp2.shortestSpan() << std::endl;
-    std::cout << sp2.longestSpan() << std::endl;
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    
+    
     return 0;
 }
